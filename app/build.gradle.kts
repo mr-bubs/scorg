a6f7ce033d498373764337007fba07212032ea62
Mr Bubs <mr.bubs.mail@gmail.com>
2026-06-02 03:41:15 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,10 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraint.layout)
     implementation(libs.androidx.core.ktx)
+    
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
 
     testImplementation(libs.junit)
 
